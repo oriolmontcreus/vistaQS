eef75d8a25e5a5daaa243a25c434adeed3d5cacb
oriolmontcreus <licensedgamer641319@gmail.com>
2024-03-14 19:07:08 +0100

    fix: fixed annoying localstorage is undefined warning

--- frontend/src/app/utils/getWithAuth.ts
+++ frontend/src/app/utils/getWithAuth.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
+declare let localStorage: any;
 
 /**
  * This function makes an authenticated HTTP GET request.
