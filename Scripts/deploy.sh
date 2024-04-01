6029b40cf08ff206cefdb6babd7e82a8dc50d076
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-01 13:40:27 +0200

    fix: fixed backend script

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -4,6 +4,9 @@
 cd "../Frontend"
 ng build --configuration=production
 
+# Step 1.5: Convert the backend deployment script to Unix format
+dos2unix "../Scripts/deploy_backend.sh"
+
 # Step 2: Transfer the built Angular app to the VM
 scp -r "../Frontend/dist/frontend" root@10.2.235.187:/home/omont/daw2/docker/frontend
 
