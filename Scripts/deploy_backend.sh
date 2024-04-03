d78ce08def2f3dd4ab84f3485ed8da4f33add776
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-03 18:32:53 +0200

    script testing

--- Scripts/deploy_backend.sh
+++ Scripts/deploy_backend.sh
@@ -3,8 +3,8 @@
 # Install Git if it's not installed
 if ! command -v git &> /dev/null
 then
-    apt-get update
-    apt-get install -y git
+        apt-get update
+        apt-get install -y git
 fi
 
 cd /home/omont/daw2/docker
@@ -17,9 +17,13 @@ cd ..
 docker-compose build
 docker-compose up -d
 
-DOCKER_TARGET_CONTAINER=docker_backend_1
+# Wait for the Docker container to be ready
+echo "Waiting for Docker container to be ready..."
+while [ "$(docker inspect -f {{.State.Running}} docker_backend_1)" != "true" ]; do
+    sleep 1
+done
 
 # Run start.sh script inside the Docker container
 echo "Running start.sh script..."
-docker cp ./start.sh $DOCKER_TARGET_CONTAINER:/var/www/html/start.sh
-docker exec $DOCKER_TARGET_CONTAINER bash -c "/var/www/html/start.sh"
+docker cp ./start.sh docker_backend_1:/var/www/html/start.sh
+docker exec docker_backend_1 bash -c "/var/www/html/start.sh"
