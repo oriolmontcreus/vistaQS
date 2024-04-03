3885352df7cfd500001faf746cd11a6f57ca3b9f
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-03 19:18:40 +0200

    testing extra seeding fix

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -15,8 +15,15 @@ composer install
 # Run migrations
 php artisan migrate --force
 
-# Seed the database
-php artisan db:seed --force
+# Check if the database has been seeded
+if [ "$(php artisan tinker --execute="return DB::table('seeded')->value('seeded');")" = "0" ]
+then
+    # Seed the database
+    php artisan db:seed --force
+
+    # Mark the database as seeded
+    php artisan tinker --execute="DB::table('seeded')->insert(['seeded' => true]);"
+fi
 
 # Start the application
 php artisan serve --host=0.0.0.0
