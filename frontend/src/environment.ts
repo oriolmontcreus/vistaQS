1418f629c4289e66d2c644919566844db195df99
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-07 17:22:33 +0200

    Working checkpoint for remote frontend/backend/database!

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,2 +1,2 @@
 // export const URI: string = 'http://localhost:8000/api';
-export const URI: string = 'http://10.2.235.187:8000/api';
+export const URI: string = 'http://10.2.235.187:8000/api'; //Target (remote server) machine
